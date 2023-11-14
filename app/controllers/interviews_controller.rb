a1919afc887913aac8552413a8e55526360ae45c
Fjkazu <fjkazu.designwork@gmail.com>
2023-11-14 20:51:44 +0900

    ランダム重複修正、質問回数１０回に修正

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -20,16 +20,21 @@ class InterviewsController < ApplicationController
   private
 
   def show_page
-    # 以前に表示したページがあれば取得、なければ初期化
-    @interviews_order = session[INTERVIEWS_ORDER_KEY] || Interview.pluck(:id).shuffle
-    @current_interview_index = session[CURRENT_INTERVIEW_INDEX_KEY] || 0
+      # 以前に表示したページがあれば取得、なければ初期化
+      @interviews_order = session[INTERVIEWS_ORDER_KEY] || Interview.pluck(:id).shuffle
+      @current_interview_index = session[CURRENT_INTERVIEW_INDEX_KEY].to_i || 0
 
-    if @current_interview_index < @interviews_order.length
+    if @current_interview_index < 10
       @current_interview_id = @interviews_order[@current_interview_index]
       @current_interview = Interview.find(@current_interview_id)
-      session[CURRENT_INTERVIEW_INDEX_KEY] = (session[CURRENT_INTERVIEW_INDEX_KEY] || 0).to_i + 1
+
+      # 一度表示したデータは削除
+      @interviews_order.delete(@current_interview_id)
+      session[INTERVIEWS_ORDER_KEY] = @interviews_order
+
+      session[CURRENT_INTERVIEW_INDEX_KEY] = (@current_interview_index + 1)
     else
-      redirect_to  interviews_finish_path
+      redirect_to interviews_finish_path
     end
   end
 
